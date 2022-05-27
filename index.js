56a2295c7047edf65f9cc5f1666fb8b180d7619a
jide <jide.fr@gmail.com>
2022-05-27 16:50:09 +0200

    change port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 8080 });
