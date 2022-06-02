97ba88a6a63f6a3dbae7d6dcdb2fa6df7183abc7
jide <jide.fr@gmail.com>
2022-06-02 13:02:40 +0200

    Trying to make this shit work

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const port = 3001;
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 8081 });
 
 let _ws;
  
@@ -18,11 +20,45 @@ wss.on('connection', ws => {
 app.use(express.static('static'));
 
 app.get('/video/:id', (req, res) => {
-  if (_ws) {
-    _ws.send(req.params.id);
+  console.log('video', req.params);
+
+  if (req.params.id !== 'pause' && !fs.existsSync(`./static/videos/${req.params.id}`)) {
+    res.send('fiel does not exist');
   }
 
-  res.send('');
+  if (_ws) {
+    _ws.send(req.params.id === 'pause' ? 'pauseVideo' : req.params.id);
+  }
+
+  res.send('ok');
+});
+
+app.get('/audio/:id', (req, res) => {
+  console.log('audio', req.params);
+
+  if (req.params.id !== 'pause' && !fs.existsSync(`./static/audios/${req.params.id}`)) {
+    res.send('fiel does not exist');
+  }
+
+  if (_ws) {
+    _ws.send(req.params.id === 'pause' ? 'pauseAudio' : req.params.id);
+  }
+
+  res.send('ok');
+});
+
+app.get('/button/:a/:b', async (req, res) => {
+  console.log('button', req.params);
+
+  try {
+    const response = await fetch(`http://192.168.0.2:5000/button/${req.params.a}/${req.params.b}`);
+    console.log(response);
+  }
+  catch(e) {
+    console.log(e);
+  }
+
+  res.send('ok');
 });
 
 app.listen(port, () => {
--- static/client.js
+++ static/client.js
@@ -1,17 +1,82 @@
-const url = 'ws://localhost:8080'
+const url = 'ws://localhost:8081'
 const connection = new WebSocket(url);
 
-var video = document.getElementById('video');
- 
+let audio;
+
+function playAudio(id) {
+  if (audio) {
+    audio.src = '';
+    audio.load();
+    audio.remove();
+    delete audio;
+  }
+  audio = document.createElement('audio');
+  audio.setAttribute('id', 'audio');
+  audio.setAttribute('src', `/audios/${id}`);
+  audio.onended = function() {
+    audio.src = '';
+    audio.load();
+    audio.remove();
+    delete audio;
+  }
+  document.body.append(audio);
+  audio.play();
+}
+
+function pauseAudio() {
+  if (audio) {
+    audio.pause();
+  }
+}
+
+let video;
+
+function playVideo(id) {
+  if (video) {
+    video.src = '';
+    video.load();
+    video.remove();
+    delete video;
+  }
+  video = document.createElement('video');
+  video.setAttribute('id', 'video');
+  video.setAttribute('preload', 'none');
+  video.setAttribute('src', `/videos/${id}`);
+  video.onended = function() {
+    video.src = '';
+    video.load();
+    video.remove();
+    delete video;
+  }
+  document.body.append(video);
+  video.play();
+}
+
+function pauseVideo() {
+  if (video) {
+    video.pause();
+  }
+}
+
 connection.onerror = (error) => {
   console.log(`WebSocket error: ${error}`)
 }
- 
+
 connection.onmessage = (e) => {
-  video.src="";
-  setTimeout(() => {
-    video.src= `/videos/${e.data}`;
-    video.muted = false;
-    video.play();
-  }, 1000);
+  if (e.data === 'pauseVideo') {
+    pauseVideo();
+  }
+  else if (e.data === 'pauseAudio') {
+    pauseAudio();
+  }
+  else {
+    const [,type] = e.data.split('.');
+
+    if (type === 'mp4') {
+      playVideo(e.data);
+    }
+    else {
+      playAudio(e.data);
+    }
+  }
 }
